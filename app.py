e8da91d2530a60c53a7b510dc7e490150cce094e
kumaoni-ladka <151643076+kumaoni-ladka@users.noreply.github.com>
2024-06-28 16:28:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+# from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 import torch
 
 # Load pretrained T5 model and tokenizer
-tokenizer = T5Tokenizer.from_pretrained('t5-small')
-model = T5ForConditionalGeneration.from_pretrained('t5-small')
+tokenizer = AutoTokenizer.from_pretrained('mrm8488/bert-tiny-5-finetuned-squadv2')
+model = AutoModelForQuestionAnswering.from_pretrained('mrm8488/bert-tiny-5-finetuned-squadv2')
 
 # Define questions and context
 questions = [
@@ -21,9 +22,7 @@ questions = [
     "What are the Clubs of Gen Garage?"
 ]
 
-context = '''
-Gen Garage was founded in Mumbai in 2019 as an innovative platform that unites fresh graduates, interns, mentors, and industry leaders. It focuses on systematically transforming talents for industrial settings and includes all Capgemini employees by default. For further engagement, individuals can connect with business line leads or the core team via the 'meet the team' page. Additionally, Gen Z Campus, a collaborative learning initiative with I&D Gen Z Arena, offers immersive learning opportunities, mentorship, and an innovators' space. Operating nationwide, Gen Garage has a significant presence in Bangalore, Mumbai, Pune, Chennai, Hyderabad, Kolkata, and Delhi NCR. It specializes in Data Engineering, Data Science, Machine Learning, Data Visualization, and Cloud Technologies. Formerly known as Millennial Garage, it underwent rebranding in 2023 to 'Gen Garage' with a federated model. Weekly activities include Knowledge Sharing Sessions on Monday, Tuesday Tips, Innovation of the Week on Wednesday, Guru Gyan on Thursday, and Masti Friday. The community features six clubs: Mantri Club, Art Lens Club, Social Media Champs, Escape Club, Magic Pen, and Falcon Club
-'''
+context = ""<b>Welcome to Gen Garage!<br><br> I'm your friendly GENIE, at your service!<br><br>  Here are some options for you to explore the website and know about it:<br></b><b>*</b> About Gen Garage<br><b>*</b> Missions of Gen Garage<br><b>*</b> Leads of Gen Garage<br><b>*</b> Current Projects<br><b>*</b> Achievements"
 
 app = Flask("__name__")
 CORS(app)
@@ -31,12 +30,29 @@ CORS(app)
 
 @app.route('/input/<text>')
 def send_output(text):
-    input_text = f"question: {text} context: {context}"
-    input_ids = tokenizer(input_text, return_tensors="pt", max_length=512, truncation=True, padding=True)['input_ids']
+    # input_text = f"question: {text} context: {context}"
+    # input_ids = tokenizer(input_text, return_tensors="pt", max_length=512, truncation=True, padding=True)['input_ids']
+    # with torch.no_grad():
+    #     outputs = model.generate(input_ids, max_length=50)
+    #     answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    inputs = tokenizer(text, context, add_special_tokens=True, return_tensors="pt")
+ 
+    # Perform inference
     with torch.no_grad():
-        outputs = model.generate(input_ids, max_length=50)
-        answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return answer
+        outputs = model(**inputs)
+        start_scores = outputs.start_logits
+        end_scores = outputs.end_logits
+ 
+    # Get the most likely answer span
+    answer_start = torch.argmax(start_scores)
+    answer_end = torch.argmax(end_scores) + 1  # Add 1 because slicing in Python doesn't include the end index
+ 
+    answer = tokenizer.decode(inputs['input_ids'][0][answer_start:answer_end])
+ 
+    # Print the question and answer
+    # print(f"Question: {question}")
+    # print(f"Answer: {answer}")
+    return answer
 
 
 if __name__ == "__main__":
